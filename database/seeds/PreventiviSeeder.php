c41be7b241cf8742ecbf0fff52c74cc48e807b4e
luigi <lmaroncelli@gmail.com>
2018-03-21 17:00:24 +0100

    ok

--- database/seeds/PreventiviSeeder.php
+++ database/seeds/PreventiviSeeder.php
@@ -17,14 +17,23 @@ class PreventiviSeeder extends Seeder
 
        foreach ($old_preventivi as $old_p) 
 	       	{
-	       	$data_servizi = substr($old_p->data_servizi,0,10);
-	       	$dal_servizi = substr($old_p->da_ora_servizi,11);
-	       	$al_servizi = substr($old_p->a_ora_servizi,11);
-	       	$p['dalle'] = $data_servizi.' '.$dal_servizi; 
-	       	$p['alle'] = $data_servizi.' '.$al_servizi; 
-	       	$p['localita'] = $p->note_servizi; 
-	       	$p['motivazioni'] = $p->rapporto_servizi; 
-	       	$p['associazione_id'] = $p->id_utenti;
+	       	if ($old_p->data_servizi == '0000-00-00 00:00:00') 
+	       		{
+	       		$p['dalle'] = null;
+	       		$p['alle'] = null;
+	       		} 
+	       	else 
+	       		{
+		       	$data_servizi = substr($old_p->data_servizi,0,10);
+		       	$dal_servizi = substr($old_p->da_ora_servizi,11);
+		       	$al_servizi = substr($old_p->a_ora_servizi,11);
+		       	$p['dalle'] = $data_servizi.' '.$dal_servizi; 
+		       	$p['alle'] = $data_servizi.' '.$al_servizi; 
+	       		}
+	       	
+	       	$p['localita'] = $old_p->note_servizi; 
+	       	$p['motivazioni'] = $old_p->rapporto_servizi; 
+	       	$p['associazione_id'] = $old_p->id_utenti;
 
 	       	$new_preventivi[] = $p;
 	       	}
